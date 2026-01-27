e2076714e7f2ab3ebd25c9031d5feef2f7585568
김택우 <darkbell991@gmail.com>
2026-01-27 16:09:53 +0900

    fix: import 누락 fix (!34)
    * fix: import 누락 fix
    
    ai module에 ai repository 누락된거 수정

--- src/modules/ai-judges/ai-judges.module.ts
+++ src/modules/ai-judges/ai-judges.module.ts
@@ -5,11 +5,12 @@ import { AiJudgeController } from './ai-judges.controller';
 import { AiJudgeService } from './ai-judges.service';
 import { AiJudgesGateway } from './ai-judges.gateway';
 import { RoomsModule } from '../rooms/rooms.module';
+import { AiJudgesRepository } from './ai-judges.repository';
 
 @Module({
   imports: [RoomsModule, HttpModule, ConfigModule],
   controllers: [AiJudgeController],
-  providers: [AiJudgeService, AiJudgesGateway],
+  providers: [AiJudgeService, AiJudgesGateway, AiJudgesRepository],
   exports: [AiJudgeService],
 })
 export class AiJudgeModule {}
