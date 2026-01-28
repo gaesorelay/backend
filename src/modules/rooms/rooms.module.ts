6c17a91c4ed88db300021282fcbfa3cef6b9ba0d
김택우 <darkbell991@gmail.com>
2026-01-28 14:33:02 +0900

    fix: 방 모듈에서 ai 서비스를 import 하는 오류 수정 (!50)
    * fix: 방 모듈에서 ai 서비스를 import 하는 오류 수정

--- src/modules/rooms/rooms.module.ts
+++ src/modules/rooms/rooms.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RoomsController } from './rooms.controller';
 import { RoomsRepository } from './rooms.repository';
 import { RoomsService } from './rooms.service';
 import { RoomsGateway } from './rooms.gateway';
-import { AiJudgeService } from '../ai-judges/ai-judges.service';
-
+import { AiJudgeModule } from '../ai-judges/ai-judges.module';
 @Module({
+  imports: [forwardRef(() => AiJudgeModule)],
   controllers: [RoomsController],
-  providers: [RoomsService, RoomsRepository, RoomsGateway, AiJudgeService],
+  providers: [RoomsService, RoomsRepository, RoomsGateway],
   exports: [RoomsService],
 })
 export class RoomsModule {}
