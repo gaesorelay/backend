2895e0f40e009b14f53c1b0901c2ffd3dad369e4
송하준 <427shg@gmail.com>
2026-01-31 17:45:53 +0900

    fix: rooms.service.ts 코드 수정

--- src/modules/rooms/rooms.service.ts
+++ src/modules/rooms/rooms.service.ts
@@ -9,6 +9,7 @@ import { GamesService } from '../games/games.service';
 import { VoteOutcome } from '../games/types/vote-outcome.type';
 import { GameFlowService } from './game-flow.service';
 import { RoomStatusSubject } from './room-status.subject';
+import { WsException } from '@nestjs/websockets';
 
 @Injectable()
 export class RoomsService {
@@ -46,6 +47,7 @@ export class RoomsService {
    */
   async joinRoom(
     roomUuid: string,
+    ip: string,
     nickname: string,
     socketId: string,
     avatarId: number,
@@ -55,6 +57,11 @@ export class RoomsService {
     const room = await this.roomsRepository.findById(roomUuid);
     if (!room) {
       throw new NotFoundException('존재하지 않는 방입니다.');
+    }
+
+    const isBanned = await this.roomsRepository.isIpBanned(roomUuid, ip);
+    if (isBanned) {
+      throw new WsException('강퇴당한 방에는 30분간 재입장할 수 없습니다. 🚫');
     }
 
     // 2. 재접속 시도 확인 (DB에 유저 정보가 있는지 체크)
@@ -114,6 +121,7 @@ export class RoomsService {
 
       avatarId: resolvedAvatarId,
       isReady: false,
+      IP: ip,
     };
 
     // 5. Redis 저장
