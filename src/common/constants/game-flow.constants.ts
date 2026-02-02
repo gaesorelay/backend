ae0cf224476c54b87a599bbfb56694602ba2b8b1
Mayonaise <genjinie7@gmail.com>
2026-02-02 15:37:45 +0900

    chore: 판사 카드 셔플 시간 조정

--- src/common/constants/game-flow.constants.ts
+++ src/common/constants/game-flow.constants.ts
@@ -1,4 +1,4 @@
 export const CARD_SHUFFLE_TIME = 15_000;
-export const JUDGE_SHUFFLE_TIME = 42_000;
+export const JUDGE_SHUFFLE_TIME = 27_000;
 export const STORY_TIME = 70_000;
 export const JUDGING_TIME = 20_000;
