b1c30811fb20ce818a587ed56eee3d32a2c32378
김민준 <genjinie7@gmail.com>
2026-02-04 19:10:45 +0900

    chore : 게임 시간 조정 (!116)
    * chore : 게임 시간 조정
    
    - 판사 셔플시간 24->23초
    - 스토리 전체 감상시간 70->65초

--- src/common/constants/game-flow.constants.ts
+++ src/common/constants/game-flow.constants.ts
@@ -1,5 +1,5 @@
 export const CARD_SHUFFLE_TIME = 10_000;
-export const JUDGE_SHUFFLE_TIME = 24_000;
-export const STORY_TIME = 70_000;
+export const JUDGE_SHUFFLE_TIME = 23_000;
+export const STORY_TIME = 65_000;
 export const JUDGING_TIME = 35_000;
 export const TURN_COUNT = 6;
